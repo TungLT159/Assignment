f3f603ef7e8eeb0a621210264726852038bf6bfa
TungLe <thanhtung12121212@gmail.com>
2022-05-18 14:52:43 +0700

    lap3.2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponents'
+import {DISHES} from './shared/dishes'
 
 class App extends Component {
+constructor(props){
+  super(props);
 
+  this.state = {
+    dishes: DISHES
+  }
+}
   render() {
     return (
       <div>
-          <Menu />
+       <Navbar dark color="dark">
+        <div className="container">
+          <NavbarBrand href="#">Trang chủ</NavbarBrand>
+        </div>
+      </Navbar>
+          <Menu dishes={this.state.dishes} />
       </div>
     );
   }
