d1fe53a715bf3e465e0827e5311362d237f9f648
TungLe <thanhtung12121212@gmail.com>
2022-05-22 23:11:59 +0700

    assignment1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-import Menu from './components/MenuComponents'
-import {DISHES} from './shared/dishes'
+import {STAFFS} from './shared/staffs'
+import StaffList from './components/StaffListComponent';
 
 class App extends Component {
 constructor(props){
   super(props);
 
   this.state = {
-    dishes: DISHES
+    staffs: STAFFS
   }
 }
   render() {
     return (
       <div>
-       <Navbar dark color="dark">
+        <Navbar dark color="dark">
         <div className="container">
-          <NavbarBrand href="#">Trang chủ</NavbarBrand>
+          <NavbarBrand href="#">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
         </div>
-      </Navbar>
-          <Menu dishes={this.state.dishes} />
+        </Navbar>
+        <StaffList staffs={this.state.staffs} />
       </div>
     );
   }
